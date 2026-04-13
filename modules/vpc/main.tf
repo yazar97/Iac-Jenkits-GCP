bde119dfe0bb48960c5bc93ea5615cd71325c276
Yazar Arafath <yazaryazu@gmail.com>
2026-04-13 13:01:11 +0530

    # in var

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "vpc" {
+#resource "google_compute_network" "vpc" {
   name                    = var.vpc_name
   auto_create_subnetworks = false
   project                 = var.project_id
